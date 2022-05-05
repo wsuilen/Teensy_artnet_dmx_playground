ee078f1e9736e9eab72da41a72185083064abc43
SuilenStudios <contact@willemsuilen.com>
2022-05-05 14:00:44 +0200

    Sprintdemo_2

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@
 #include "Ledstrip.h"
 
 const uint8_t ledpin = 7; // hoe krijg ik deze toegankelijk in classes?
-// #define NUM_LEDS 13
-// #define NUM_STRIPS 1
+#define NUM_LEDS 13
+#define NUM_STRIPS 1
 
 // Ledstrip ledstrip_1(13, 7);
 
@@ -19,15 +19,10 @@ const int amount_of_leds = 13;
 CRGB leds[amount_of_leds];
 
 Artnet artnet;
-byte ip[] = {192, 168, 0, 199};        // Teensy
+byte ip[] = {192, 168, 0, 190};        // Teensy
 byte broadcast[] = {192, 168, 0, 255}; // Broadcast address
 byte mac[] = {0x04, 0xE9, 0xE5, 0x00, 0x69, 0xEC};
 uint16_t universe = 1;
-
-uint8_t dmxFrame1 = 0;
-uint8_t dmxFrame2 = 0;
-uint8_t dmxFrame3 = 0;
-uint8_t dmxFrame4 = 0;
 
 int artnet_length = artnet.getLength();
 
@@ -45,17 +40,11 @@ void setup()
   FastLED.setMaxPowerInVoltsAndMilliamps(12, 500);
   fill_solid(leds, 13, CRGB::Black);
   FastLED.show();
-
 }
 
 void loop()
 {
-
   uint16_t incoming_packet = artnet.read();
-
-  // if (incoming_packet == ART_POLL) { // checks if incoming_packet contains the art_net protocol
-  //   Serial.println("Poll");
-  // }
 
   if (incoming_packet == ART_DMX && artnet.getUniverse() == universe)
   {
